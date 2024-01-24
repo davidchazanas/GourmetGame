6d664bfbd45197f8dcaad130d4e8223cdb2a4d88
David <david.chazanas@paradigmabs.com.br>
2024-01-24 14:31:24 -0300

    posiciona imagens e labels no dialog

--- CenteredMessageBox.cs
+++ CenteredMessageBox.cs
@@ -5,13 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace GourmetGame
 {
     
     public static class CenteredMessageBox
     {
-        public static DialogResult Show(string text, string caption)
+        public static DialogResult Show(string text, string caption, bool onlyOkButton)
         {
             Form prompt = new Form()
             {
@@ -22,15 +23,33 @@ namespace GourmetGame
                 StartPosition = FormStartPosition.CenterParent,
                 AutoSize = true
             };
-            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, AutoSize=true};
-            Button yesButton = new Button() { Text = "Yes", Left = 50, Width = 50, Top = 70, DialogResult = DialogResult.Yes };
-            yesButton.Click += (sender, e) => { prompt.Close(); };
-            Button noButton = new Button() { Text = "No", Left = 150, Width = 50, Top = 70, DialogResult = DialogResult.No };
-            noButton.Click += (sender, e) => { prompt.Close(); };
-            prompt.Controls.Add(yesButton);
-            prompt.Controls.Add(noButton);
+            Label textLabel = new Label() { Left = 62, Top = 30, Text = text, AutoSize=true, Font = new Font(Label.DefaultFont, FontStyle.Bold) };
+            PictureBox image = new PictureBox() 
+            {
+                Image = global::GourmetGame.Properties.Resources.question_logo,
+                Location = new System.Drawing.Point(12, 19),
+                Size = new System.Drawing.Size(52, 59)
+            };
+
+            if (!onlyOkButton)
+            {
+                Button yesButton = new Button() { Text = "Yes", Left = 65, Width = 50, Top = 70, DialogResult = DialogResult.Yes };
+                yesButton.Click += (sender, e) => { prompt.Close(); };
+                Button noButton = new Button() { Text = "No", Left = 165, Width = 50, Top = 70, DialogResult = DialogResult.No };
+                noButton.Click += (sender, e) => { prompt.Close(); };
+                prompt.Controls.Add(yesButton);
+                prompt.Controls.Add(noButton);
+                prompt.AcceptButton = yesButton;
+            } else
+            {
+                Button okButton = new Button() { Text = "Ok", Left = 165, Width = 50, Top = 70, DialogResult = DialogResult.OK };
+                okButton.Click += (sender, e) => { prompt.Close(); };
+                prompt.Controls.Add(okButton);
+                image.Image = global::GourmetGame.Properties.Resources.exclamlogo;
+            }
+            
+            prompt.Controls.Add(image);
             prompt.Controls.Add(textLabel);
-            prompt.AcceptButton = yesButton;
 
             return prompt.ShowDialog() == DialogResult.Yes ? DialogResult.Yes : DialogResult.No;
         }
--- Prompt.cs
+++ Prompt.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,10 +23,19 @@ namespace GourmetGame
                 AutoSize = true
             };
 
-            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, AutoSize=true };
-            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 200 };
-            Button confirmation = new Button() { Text = "OK", Left = 50, Width = 50, Top = 70, DialogResult = DialogResult.OK };
-            Button cancel = new Button() { Text = "Cancelar", Left = 150, Width = 50, Top = 70, DialogResult = DialogResult.Cancel };
+
+            PictureBox image = new PictureBox()
+            {
+                Image = global::GourmetGame.Properties.Resources.question_logo,
+                Location = new System.Drawing.Point(12, 19),
+                Size = new System.Drawing.Size(52, 59)
+            };
+
+
+            Label textLabel = new Label() { Left = 62, Top = 20, Text = text, AutoSize=true, Font = new Font(Label.DefaultFont, FontStyle.Bold) };
+            TextBox textBox = new TextBox() { Left = 65, Top = 50, Width = 200 };
+            Button confirmation = new Button() { Text = "OK", Left = 65, Width = 50, Top = 85, DialogResult = DialogResult.OK };
+            Button cancel = new Button() { Text = "Cancelar", Left = 165, Width = 50, Top = 85, DialogResult = DialogResult.Cancel };
             confirmation.Click += (sender, e) => { prompt.Close(); };
             cancel.Click += (sender, e) => { prompt.Close(); };
             prompt.Controls.Add(textBox);
@@ -33,6 +43,7 @@ namespace GourmetGame
             prompt.Controls.Add(cancel);
             prompt.Controls.Add(textLabel);
             prompt.AcceptButton = confirmation;
+            prompt.Controls.Add(image);
 
             return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
         }
